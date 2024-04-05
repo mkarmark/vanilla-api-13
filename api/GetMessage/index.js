9c9ed6006effcb184ca1af425baf920a639d5b7a
mkarmark <mikarmar@microsoft.com>
2024-04-05 15:46:33 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API" },
+    body: { text: "Hello from the API 4/5/24 3:46pm" },
   };
 };
